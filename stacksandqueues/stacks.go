baa0b60ea312003c48f006376a6daf78dcefa47f
mtdamir <amir.motavas79@gmail.com>
2023-08-13 18:47:51 +0430

    stacksandqueues

---
+++ stacksandqueues/stacks.go
@@ -0,0 +1,33 @@
+// package stacksandqueues
+
+// import "fmt"
+
+// type Stack struct {
+// 	items []int
+// }
+
+// // Push will add a value at the end
+// func (s *Stack) Push(i int) {
+// 	s.items = append(s.items, i)
+// }
+
+// // Pop will remove a value at the end
+// // and returns the remove value
+// func (s *Stack) Pop() int {
+// 	l := len(s.items) - 1
+// 	toRemove := s.items[l]
+// 	s.items = s.items[:l]
+// 	return toRemove
+// }
+
+// func main() {
+// 	myStack := Stack{}
+// 	fmt.Println(myStack)
+// 	myStack.Push(100)
+// 	myStack.Push(200)
+// 	myStack.Push(300)
+// 	fmt.Println(myStack)
+
+// 	myStack.Pop()
+// 	fmt.Println(myStack)
+// }
